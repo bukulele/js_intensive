085d867334ef807e51937a47fe11be70d426657b
bukulele <kit.sazonov@gmail.com>
2022-04-25 12:59:28 +0400

    selectFromInterval done

---
+++ selectFromInterval.js
@@ -0,0 +1,24 @@
+function selectFromInterval(arr, firstValue, secondValue) {
+	try {
+		if (
+			!(arr instanceof Array) ||
+			isNaN(firstValue) ||
+			isNaN(secondValue) ||
+			arr.some((val) => isNaN(val))
+		) {
+			throw new Error('Некорректный ввод!');
+		} else {
+			let resultArr = [];
+			let intStart = firstValue < secondValue ? firstValue : secondValue;
+			let intEnd = firstValue < secondValue ? secondValue : firstValue;
+			for (let i = 0; i < arr.length; i++) {
+				if (arr[i] >= intStart && arr[i] <= intEnd) {
+					resultArr.push(arr[i]);
+				}
+			}
+			return resultArr;
+		}
+	} catch (error) {
+		console.log(error.message);
+	}
+}
